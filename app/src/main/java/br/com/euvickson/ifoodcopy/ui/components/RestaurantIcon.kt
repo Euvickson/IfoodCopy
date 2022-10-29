8b47f6daf8c8223224de9cc7efeceaa095f0b30c
Euvickson <victor.eugennio@gmail.com>
2022-10-29 09:36:41 -0300

    Added components to the RestaurantList

--- app/src/main/java/br/com/euvickson/ifoodcopy/ui/components/RestaurantIcon.kt
+++ app/src/main/java/br/com/euvickson/ifoodcopy/ui/components/RestaurantIcon.kt
@@ -1,6 +1,7 @@
 package br.com.euvickson.ifoodcopy.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
--- app/src/main/java/br/com/euvickson/ifoodcopy/ui/components/RestaurantList.kt
+++ app/src/main/java/br/com/euvickson/ifoodcopy/ui/components/RestaurantList.kt
@@ -1,10 +1,12 @@
 package br.com.euvickson.ifoodcopy.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.euvickson.ifoodcopy.model.Profile
@@ -13,13 +15,26 @@ import br.com.euvickson.ifoodcopy.R
 
 @Composable
 fun RestaurantList(profile: Profile) {
-    LazyRow(
-        contentPadding = PaddingValues(horizontal = 8.dp),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        profile.lastRestaurantList?.let { restaurantList ->
-            items(restaurantList) { restaurant ->
-                RestaurantIcon(restaurant = restaurant)
+
+    Column (verticalArrangement = Arrangement.spacedBy(8.dp)) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "Últimas Lojas")
+            Text(text = "Ver mais", color = Color.Red)
+        }
+
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            profile.lastRestaurantList?.let { restaurantList ->
+                items(restaurantList) { restaurant ->
+                    RestaurantIcon(restaurant = restaurant)
+                }
             }
         }
     }
@@ -58,4 +73,5 @@ fun RestaurantListPreview() {
             )
         )
     )
+
 }
