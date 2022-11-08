b82c130f8a2c74ad981caf922a1a9b1189f3fecc
Euvickson <victor.eugennio@gmail.com>
2022-11-07 22:43:59 -0300

    vertical Scroll applied to all elements of the screen - Done
    To Do: Code Refactor

--- app/src/main/java/br/com/euvickson/ifoodcopy/ui/components/lists/ListOfRestaurantDetailCard.kt
+++ app/src/main/java/br/com/euvickson/ifoodcopy/ui/components/lists/ListOfRestaurantDetailCard.kt
@@ -9,14 +9,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.euvickson.ifoodcopy.model.Restaurant
+import br.com.euvickson.ifoodcopy.ui.components.RestaurantList
+import br.com.euvickson.ifoodcopy.ui.components.TabBar
 import br.com.euvickson.ifoodcopy.ui.components.icons.RestaurantDetailCard
+import br.com.euvickson.ifoodcopy.ui.components.samples.listOfIconsSample
+import br.com.euvickson.ifoodcopy.ui.components.samples.listOfProfileSample
 import br.com.euvickson.ifoodcopy.ui.components.samples.listOfRestaurantSample
 
 @Composable
 fun ListOfRestaurantDetailCard(listOfRestaurants: List<Restaurant>) {
     Column (verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Text(text = "Lojas")
         LazyColumn (verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            item {
+                TabBar(tabBarIcons = listOfIconsSample)
+                CardList()
+                RestaurantList(listOfProfileSample)
+                Text(text = "Lojas")
+            }
             items(listOfRestaurants) { restaurant ->
                 RestaurantDetailCard(restaurant = restaurant)
             }
--- app/src/main/java/br/com/euvickson/ifoodcopy/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/euvickson/ifoodcopy/ui/screens/HomeScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.euvickson.ifoodcopy.ui.components.RestaurantList
 import br.com.euvickson.ifoodcopy.ui.components.TabBar
-import br.com.euvickson.ifoodcopy.ui.components.icons.RestaurantDetailCard
 import br.com.euvickson.ifoodcopy.ui.components.lists.CardList
 import br.com.euvickson.ifoodcopy.ui.components.lists.ListOfRestaurantDetailCard
 import br.com.euvickson.ifoodcopy.ui.components.samples.listOfIconsSample
@@ -17,9 +16,6 @@ import br.com.euvickson.ifoodcopy.ui.components.samples.listOfRestaurantSample
 @Composable
 fun StartScreen() {
     Column (verticalArrangement = Arrangement.spacedBy(5.dp)) {
-        TabBar(tabBarIcons = listOfIconsSample)
-        CardList()
-        RestaurantList(listOfProfileSample)
         ListOfRestaurantDetailCard(listOfRestaurants = listOfRestaurantSample)
     }
 }
